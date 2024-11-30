c78dcb4e7728f2b06d795b1fc7e9fd794303e11e
abbouformations <135717843+abbouformations@users.noreply.github.com>
2024-11-30 12:39:33 +0100

    FC

---
+++ src/main/java/ma/formations/spring/rest/MainApplication.java
@@ -0,0 +1,27 @@
+package ma.formations.spring.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class MainApplication extends SpringBootServletInitializer {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MainApplication.class);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApplication.class, args);
+		System.out.println("Application démarrée");
+	}
+
+}
